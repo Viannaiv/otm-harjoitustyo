3392a4686f239a7fc8400d3062baebb08640332f
Viannaiv <viva95@windowslive.com>
2018-04-23 15:50:13 +0300

    Corrected and added methods to Field

--- Minesweeper/src/main/java/minesweeper/domain/Field.java
+++ Minesweeper/src/main/java/minesweeper/domain/Field.java
@@ -1,13 +1,14 @@
 
 package minesweeper.domain;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 //This might not be the final solution if i figure out something that works better
-//This will not be the final solution...
 
-public class Field { //should I check that values are positive
+public class Field { //should I check that values are positive?
     private int rows;
     private int columns;
     private int mines;
@@ -22,12 +23,8 @@ public class Field { //should I check that values are positive
         this.random = new Random();
         
         createTiles();
-        //NOTE: These methods don't work as planned and i 
-//        didnt yet find the reason... So i commented them so that
-//        the test at leastwork...
-
         addMinesToField();
-//        setMinesNearForTiles();
+        setMinesNearForTiles();
     }
     
     private void createTiles() {
@@ -62,172 +59,73 @@ public class Field { //should I check that values are positive
         }
     }
     
+    public List<Tile> getNeighbouringTiles(int x, int y) {
+        List<Tile> neighbours = new ArrayList<>();
+        
+        if (y + 1 < this.rows) {
+            neighbours.add(this.tiles.get(x).get(y + 1));
+            
+            if (x - 1 >= 0) {
+                neighbours.add(this.tiles.get(x - 1).get(y + 1));
+            }
+            
+            if (x + 1 < this.columns) {
+                neighbours.add(this.tiles.get(x + 1).get(y + 1));
+            }
+        }
+        
+        if (x + 1 < this.columns) {
+            neighbours.add(this.tiles.get(x + 1).get(y));
+        }
+        
+        if (x - 1 >= 0) {
+            neighbours.add(this.tiles.get(x - 1).get(y));
+        }
+        
+        if (y - 1 >= 0) {
+            neighbours.add(this.tiles.get(x).get(y - 1));
+            
+            if (x - 1 >= 0) {
+                neighbours.add(this.tiles.get(x - 1).get(y - 1));
+            }
+            
+            if (x + 1 < this.columns) {
+                neighbours.add(this.tiles.get(x + 1).get(y - 1));
+            }
+        }
+        
+        return neighbours;
+    }
+    
     //Maybe this should be counted separately only when required?
     // especially if I set the first tile clicked
     // to be clear even if originaly mined, thus changing minesNear for some tiles? 
     //Then it would not be needed in tiles?
     // anyway this is a problem for later!
-//    private void setMinesNearForTiles() {
-//        /*
-//        Currently only makes changes for tiles that are empty.
-//        Since mined tiles do not need this information, considering they do not
-//        show a number when opened.
-//        */
-//        setMinesNearInCenterTiles();
-//        setMinesNearInSideTiles();
-//        setMinesNearInCornerTiles();
-//    }
-//    
-//    // make this more reasonable later. Just like the others too. 
-//    // This should be done only when needed.
-//    //I AM AWARE this is not very efficient way to handle the problem
-//    private void setMinesNearInCenterTiles() {
-//        for (int y = 1; y < this.rows - 1; y++) {
-//            
-//            for (int x = 1; x < this.columns - 1; x++) {
-//                int minesNear = 0;
-//                Tile tile = tiles.get(x).get(y);
-//                if (!tile.isMined()) {
-//                    for (int y2 = y + 1; y2 >= y - 1; y2--) {
-//                        for (int x2 = x - 1; x2 <= x + 1; x2++) {
-//                            if (tiles.get(x2).get(y2).isMined()) {
-//                                minesNear++;
-//                            }
-//                        }
-//                    }
-//                    
-//                    tile.setMinesNear(minesNear);
-//                }
-//            }
-//        }
-//    }
-//    
-//    //Maybe make a method getNeigbouringTiles() if needed
-//    
-//    private void setMinesNearInSideTiles() {
-//        //from (0.1) to (0.(this.rows-2))
-//        for (int y = 1; y < this.rows - 1; y++) {
-//            int minesNear = 0;
-//            Tile tile = tiles.get(0).get(y);
-//            if (!tile.isMined()) {
-//                for (int y2 = y - 1; y2 <= y + 1; y2++) {
-//                    for (int x = 0; x <= x + 1; x++) {
-//                        if (tiles.get(x).get(y2).isMined()) {
-//                            minesNear++;
-//                        }
-//                    }
-//                }
-//                tile.setMinesNear(minesNear);
-//            }
-//        }
-//        
-//        //from ((this.columns - 1).1) to ((this.columns - 1).(this.rows -2))
-//        for (int y = 1; y < this.rows - 1; y++) {
-//            int minesNear = 0;
-//            Tile tile = tiles.get(this.columns - 1).get(y);
-//            if (!tile.isMined()) {
-//                for (int y2 = y - 1; y2 <= y + 1; y2++) {
-//                    for (int x = this.columns - 1; x >= x - 1; x--) {
-//                        if (tiles.get(x).get(y2).isMined()) {
-//                            minesNear++;
-//                        }
-//                    }
-//                }
-//                tile.setMinesNear(minesNear);
-//            }
-//        }
-//        
-//        //from (1.0) to ((this.columns - 2).0) to 
-//        for (int x = 1; x < this.columns - 1; x++) {
-//            int minesNear = 0;
-//            Tile tile = tiles.get(x).get(0);
-//            if (!tile.isMined()) {
-//                for (int x2 = x - 1; x2 <= x + 1; x2++) {
-//                    for (int y = 0; y <= y + 1; y++) {
-//                        if (tiles.get(x2).get(y).isMined()) {
-//                            minesNear++;
-//                        }
-//                    }
-//                }
-//                tile.setMinesNear(minesNear);
-//            }
-//        }
-//        
-//        //from (1.((rows -2)) to ((columns -2).(rows -2))
-//        for (int x = 1; x < this.columns - 1; x++) {
-//            int minesNear = 0;
-//            Tile tile = tiles.get(x).get(this.rows - 1);
-//            if (!tile.isMined()) {
-//                for (int x2 = x - 1; x2 <= x + 1; x2++) {
-//                    for (int y = this.rows - 1; y >= y - 1; y--) {
-//                        if (tiles.get(x2).get(y).isMined()) {
-//                            minesNear++;
-//                        }
-//                    }
-//                }
-//                tile.setMinesNear(minesNear);
-//            }
-//        }
-//    }
-//    
-//    private void setMinesNearInCornerTiles() {
-//        //(0.0)
-//        int minesNear = 0;
-//        Tile tile = tiles.get(0).get(0);
-//        if (!tile.isMined()) {
-//            for (int y = 0; y <= 1; y++) {
-//                for (int x = 0; x <= 1; x++) {
-//                    if (tiles.get(x).get(y).isMined()) {
-//                        minesNear++;
-//                    }
-//                }
-//            }
-//            tile.setMinesNear(minesNear);
-//        }
-//        
-//        //(0.(rows - 1))
-//        minesNear = 0;
-//        tile = tiles.get(0).get(this.rows - 1);
-//        if (!tile.isMined()) {
-//            for (int y = this.rows - 1; y >= this.rows - 2; y--) {
-//                for (int x = 0; x <= 1; x++) {
-//                    if (tiles.get(x).get(y).isMined()) {
-//                        minesNear++;
-//                    }
-//                }
-//            }
-//            tile.setMinesNear(minesNear);
-//        }
-//        
-//        //((columns - 1).0)
-//        minesNear = 0;
-//        tile = tiles.get(this.columns - 1).get(0);
-//        if (!tile.isMined()) {
-//            for (int y = 0; y <= 1; y++) {
-//                for (int x = this.columns - 1; x >= this.columns - 2; x--) {
-//                    if (tiles.get(x).get(y).isMined()) {
-//                        minesNear++;
-//                    }
-//                }
-//            }
-//            tile.setMinesNear(minesNear);
-//        }
-//        
-//        //((columns -1).(rows - 1))
-//        minesNear = 0;
-//        tile = tiles.get(this.columns - 1).get(this.rows - 1);
-//        if (!tile.isMined()) {
-//            for (int y = this.rows - 1; y >= this.rows - 2; y--) {
-//                for (int x = this.columns - 1; x >= this.columns - 2; x--) {
-//                    if (tiles.get(x).get(y).isMined()) {
-//                        minesNear++;
-//                    }
-//                }
-//            }
-//            tile.setMinesNear(minesNear);
-//        }
-//    }
-
+    
+    private void setMinesNearForTiles() {
+        /*
+        Currently only makes changes for tiles that are empty.
+        Since mined tiles do not need this information, considering they do not
+        show a number when opened.
+        */
+        
+        for (int y = 0; y < this.rows; y++) {
+            
+            for (int x = 0; x < this.columns; x++) {
+                if (!this.tiles.get(x).get(y).isMined()) {
+                    List<Tile> neighbours = getNeighbouringTiles(x, y);
+                
+                    long minesNear = neighbours.stream()
+                                               .filter(tile -> tile.isMined() == true)
+                                               .count();
+                    
+                    this.tiles.get(x).get(y).setMinesNear((int) minesNear);
+                }
+            }
+        }
+    }
+    
     public int getRows() {
         return rows;
     }
@@ -244,6 +142,6 @@ public class Field { //should I check that values are positive
         return tiles;
     }
     
-    
+    //Should there be a get with xCoords method?
     
 }
