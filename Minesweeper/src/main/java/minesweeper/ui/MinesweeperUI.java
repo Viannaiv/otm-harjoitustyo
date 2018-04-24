2127f870e209dc7e0a4af001f2ed30e502f1d699
Viannaiv <viva95@windowslive.com>
2018-04-24 19:34:06 +0300

    now possible to generate an executable jar file of the app

--- Minesweeper/src/main/java/minesweeper/ui/MinesweeperUI.java
+++ Minesweeper/src/main/java/minesweeper/ui/MinesweeperUI.java
@@ -2,7 +2,6 @@
 package minesweeper.ui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
